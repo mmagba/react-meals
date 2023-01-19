e96e15845c4ee89910d19960d2766cdde1fbb4c3
Mahmoud Moaad <mahmoud.moaad@gmail.com>
2023-01-20 00:24:45 +0200

    Add description section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Layout/Header';
+import MealsSummary from './components/Meals/MealsSummary';
 
 function App() {
   return (
     <>
       <Header></Header>
+      <MealsSummary />
     </>
   );
 }
